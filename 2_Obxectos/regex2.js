9e4aac53f563e30f224129c5380fd9ec2f678ef2
a21inespg <a21inespg@iessanclemente.net>
2022-11-10 13:54:46 +0100

    hah

--- 2_Obxectos/regex2.js
+++ 2_Obxectos/regex2.js
@@ -1,23 +1,24 @@
 "use strict";
 
 // Exercicio 2
-let regex2 = /\b([0-9A-F]{2}:){5}([0-9A-F]{2})$\b/i;
+let regex2 = /^([0-9A-F]{2}:){5}([0-9A-F]{2})$/i;
 let macProba = "01:32:54:67:89:AB";
 console.log(regex2.test(macProba));
 
 //Exercicio 3
-let regex3 = /#([A-Z0-9]{3}){1,2}\b/i;
+let regex3 = /^#([A-Z0-9]{3}){1,2}\b/i;
 let color = "#010775";
-console.log(regex3.test(color));
+console.log("REgex : " + regex3.test(color));
 
 //Exercicio 4
-let regex4 = /([-.+]?\d+[.]?\d*)/g;
-let numeros = "-1.2 4 0 -123.5";
+let regex4 = /\b([-+]?\d+([.]\d+)?)\b/g;
+let numeros = "-1.2 4 0 -123.5 6.s";
 console.log(numeros.match(regex4));
-
+console.log(regex4.test(numeros));
 //Exercicio 5
 
 function contrasinalValido(contrasinal) {
+  // con ??= podese facer que cumpla todas as condicións
   let contador = 0;
   if (/([^\s]){8,}/.test(contrasinal)) {
     contador++;
@@ -30,7 +31,7 @@ function contrasinalValido(contrasinal) {
     if (/[0-9]+/.test(contrasinal)) {
       contador++;
     }
-    if (/[¡!$?%&#@/()=¿?*[;,:._<>-]+/.test(contrasinal)) {
+    if (/[¡!$/?%&#@/()+=¿?*[;,:._<>-]/.test(contrasinal)) {
       contador++;
     }
     console.log("ConTADOR " + contador);
@@ -53,7 +54,7 @@ function eliminarEtiquetas(texto) {
 
 console.log(eliminarEtiquetas("<h1>sdjfhsdkjfhsdkfs <p>dsjdsjdn</p></h1>"));
 
-// Exercicio 7
+// Exercicio 7 - O MEU
 function asteriscosInsultos(texto) {
   let insultos = [
     "testán",
@@ -76,6 +77,30 @@ function asteriscosInsultos(texto) {
   return palabras.join(" ");
 }
 
+// Exercicio 7 - O  CORREXIDO
+
 console.log(
   asteriscosInsultos("hola que tal pedazo de langrán y fervellasverzas")
 );
+
+let insultos = [
+  "testan",
+  "langrán",
+  "fervellasverzas",
+  "baldreu",
+  "lacazán",
+  "pillabán",
+];
+let stringInsultos = insultos.join("|");
+function substituirInsulto(match) {
+  return (
+    match.substring(0, 1) + match.substring(1, match.length).replace(/./g, "*")
+  );
+}
+
+let texto =
+  "hola que tal pedazo de testan y fervellasverzas e lacazán e pillabán";
+console.log("Texto insultos " + texto);
+let patronInsultos = new RegExp(stringInsultos, "gi");
+let textoSaida = texto.replace(patronInsultos, substituirInsulto);
+console.log("Texto saida " + textoSaida);
