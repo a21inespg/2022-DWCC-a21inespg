5d18b653233d0d580b9f19bcb024a066ce2d211e
a21inespg <a21inespg@iessanclemente.net>
2022-11-14 17:57:07 +0100

    Comenzando co DOM

---
+++ 3_DOM/Ficheiros fonte para exercicios-20221114/02-accesoNodos02.js
@@ -0,0 +1,10 @@
+console.log(document.getElementById("input2"));
+console.log(document.getElementsByTagName("p"));
+console.log(document.getElementById("lipsum").getElementsByTagName("p"));
+console.log(document.getElementsByTagName("form"));
+console.log(document.getElementsByTagName("input"));
+console.log(document.getElementsByName("sexo"));
+
+let lis = document.getElementsByTagName("ul")[0];
+console.log(lis.getElementsByClassName("important"));
+console.log(document.getElementsByClassName("important"));
