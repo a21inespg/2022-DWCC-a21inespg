5cb87be15999b7e4058d03f3fb117acb54fff013
a21inespg <a21inespg@iessanclemente.net>
2022-11-21 17:56:47 +0100

    jdaj

--- 3_DOM/modificandoDocumento/ex1-2-3.js
+++ 3_DOM/modificandoDocumento/ex1-2-3.js
@@ -1,14 +1,14 @@
 //  Exercicio 1: a)
-
+console.log("hola");
 console.log(document.body.getElementsByTagName("li"));
-let lis=document.body.getElementsByTagName("li");
-
+let lis = document.body.getElementsByTagName("li");
+let ol = document.body.getElementsByTagName("ol")[0];
 // Exercicio 2
 for (let li of lis) {
-    console.log(li);
-    li.remove();
+  li.remove();
 }
 
+console.log(document.body);
 // Exercicio 3
 let taboa = document.getElementById("taboa");
 taboa.remove();
--- 3_DOM/modificandoDocumento/ex6.js
+++ 3_DOM/modificandoDocumento/ex6.js
@@ -1,94 +1,83 @@
 let data = {
-    "Fish": {
-    "trout": {},
-    "salmon": {}
+  Fish: {
+    trout: {},
+    salmon: {},
+  },
+  Tree: {
+    Huge: {
+      sequoia: {},
+      oak: {},
     },
-    "Tree": {
-    "Huge": {
-    "sequoia": {},
-    "oak": {}
+    Flowering: {
+      "apple tree": {},
+      magnolia: {},
     },
-    "Flowering": {
-    "apple tree": {},
-    "magnolia": {}
-    }
+  },
+  COche: {
+    casa: {},
+    abedul: {},
+    liquidambar: {
+      sequoia: {},
+      oak: {},
     },
-    "COche": {
-        "trout": {},
-        "salmon": {}
+  },
+  camion: {
+    roda: {},
+    ventana: {},
+  },
+};
+
+// este está mal
+function createTree(data) {
+  let array = Object.entries(data);
+
+  let ol = document.createElement("ul");
+
+  for (const [nome, tipo] of array) {
+    let li = document.createElement("li");
+    li.append(nome);
+    ol.append(li);
+    array2 = Object.entries(tipo);
+
+    let ol2 = document.createElement("ul");
+    for (const [key, value] of array2) {
+      console.log("key: " + key + " value: " + value);
+      let li2 = document.createElement("li");
+      li2.append(key);
+      ol2.append(li2);
+      array3 = Object.keys(value);
+
+      if (array3.length != 0) {
+        let ol3 = document.createElement("ul");
+        for (let key2 of array3) {
+          console.log(key2);
+          let li3 = document.createElement("li");
+          li3.append(key2);
+          ol3.append(li3);
         }
-    };
-function crearLista(array) {
-    let ol=document.createElement("ul");
-    for (const [nome, tipo] of array) {
-        let li=document.createElement("li");
-        li.append(nome)
-        ol.append(li);
-        array2=Object.entries(tipo);
+        li2.append(ol3);
+      }
     }
-
-
+    li.append(ol2);
+  }
+  document.body.append(ol);
 }
 
-function createTree(data){
-    let array=Object.entries(data);
- 
+// este está ben
+function crearLista(data) {
+  let array = Object.entries(data);
+  let ol = document.createElement("ul");
 
-  
-    let ol=document.createElement("ul");
-    
-    for (const [nome, tipo] of array) {
-        let li=document.createElement("li");
-        li.append(nome)
-        ol.append(li);
-        array2=Object.entries(tipo);
-
-        let ol2=document.createElement("ul");
-            for (const [key, value] of array2) {
-                let li2=document.createElement("li");
-                li2.append(key);
-                ol2.append(li2);
-                array3=Object.keys(value);
-                
-
-                if (array3.length!=0) {
-                    let ol3=document.createElement("ul");
-                    for (let key2 of array3) {
-                        console.log(key2);
-                        let li3=document.createElement("li");
-                        li3.append(key2);
-                        ol3.append(li3);
-                    }
-                    li2.append(ol3);
-
-                    
-                }
-                
-
-            }
-        li.append(ol2);
-
-    
-    
+  for ([chave, valor] of array) {
+    let li = document.createElement("li");
+    li.append(chave);
+    ol.append(li);
+    if (Object.entries(valor) != null) {
+      li.append(crearLista(valor));
     }
-document.body.append(ol);
+  }
 
-
+  return ol;
 }
 
-createTree(data);
-
-// createTree2(data);
-
-
-function createTree2(data) {
-    ol=crearLista(Object.entries(data));
-    values=Object.values(data);
-
-    for (value of values) {
-        ol2=crearLista(Object.entries(values))
-        ol.append(ol2);
-    }
-
-    document.body.append(ol);
-}
+document.body.append(crearLista(data));
