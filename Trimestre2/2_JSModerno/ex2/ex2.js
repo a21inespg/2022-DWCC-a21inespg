23eec8bda5f0b91aa6fd7a5d3b47b8af018a2450
a21inespg <a21inespg@iessanclemente.net>
2023-02-07 10:24:26 +0100

    s

--- Trimestre2/2_JSModerno/ex2/ex2.js
+++ Trimestre2/2_JSModerno/ex2/ex2.js
@@ -1,8 +1,8 @@
-let lat,
-  lng,
-  marker = null;
+let lat, lng;
+let marker;
 let map;
-
+let arrayObxectos;
+let marcadores = [];
 function storageAvailable(type) {
   let storage;
   try {
@@ -37,10 +37,14 @@ function crearCarta(descricion, lat, lng) {
   let carta = document.createElement("div");
   carta.setAttribute("class", "card mb-2 shadow " + descricion);
   carta.innerHTML =
-    "<div class='card-body'> <div class='row'><h5 class='card-title col-11'></h5>     <button type='button' class='btn-close col-1' aria-label='Close'></button> </div> <p class='card-text' ></p> <a href='#' class='btn btn-primary'>Centrar<a></div>";
+    "<div class='card-body'> <div class='row'><h5 class='card-title col-9 col-md-11'></h5>     <button type='button' class='btn-close col-1' aria-label='Close'></button> </div> <p class='card-text' ></p> <a href='#' class='btn btn-primary'>Centrar<a></div>";
 
-  carta.getElementsByTagName("a")[0].classList.add(descricion);
-  carta.getElementsByTagName("button")[0].classList.add(descricion);
+  carta
+    .getElementsByTagName("a")[0]
+    .classList.add(descricion.replaceAll(" ", "_"));
+  carta
+    .getElementsByTagName("button")[0]
+    .classList.add(descricion.replaceAll(" ", "_"));
   let titulo = carta.getElementsByClassName("card-title")[0];
   titulo.innerHTML = descricion;
 
@@ -61,6 +65,7 @@ formulario.style.display = "none";
 formulario.addEventListener("keypress", (e) => {
   if (e.key == "Enter") {
     marker.addTo(map);
+    marcadores.push(marker);
     e.preventDefault();
     let descricion = input.value;
     crearCarta(descricion, lat, lng);
@@ -68,18 +73,21 @@ formulario.addEventListener("keypress", (e) => {
     formulario.style.display = "none";
 
     //--------------------------------//
-
     let puntos = JSON.parse(localStorage.getItem("puntos"));
-    let arrayObxectos;
-    if (puntos == null) {
+    if (puntos == undefined) {
       arrayObxectos = [];
     } else {
       arrayObxectos = puntos;
     }
-    let objeto = { nome: descricion, latitude: lat, lonxitude: lng };
+    let objeto = {
+      nome: descricion,
+      latitude: lat,
+      lonxitude: lng,
+    };
+
     arrayObxectos.push(objeto);
+
     localStorage.setItem("puntos", JSON.stringify(arrayObxectos));
-    console.log(JSON.parse(localStorage.getItem("puntos")));
   }
 });
 
@@ -88,13 +96,41 @@ document.body.addEventListener("click", (e) => {
   if (bot.tagName == "A") {
     let puntos = JSON.parse(localStorage.getItem("puntos"));
     for (punto of puntos) {
-      if (bot.classList.contains(punto.nome)) {
+      if (bot.classList.contains(punto.nome.replaceAll(" ", "_"))) {
         map.setView([punto.latitude, punto.lonxitude], 8);
         var popup = L.popup()
           .setLatLng([punto.latitude, punto.lonxitude])
           .setContent("Sitio: " + punto.nome)
           .openOn(map);
       }
+    }
+  }
+
+  if (bot.tagName == "BUTTON") {
+    if (bot.classList.contains("btn-close")) {
+      console.log(arrayObxectos);
+
+      for (obx of arrayObxectos) {
+        if (bot.classList.contains(obx.nome.replaceAll(" ", "_"))) {
+          let index = arrayObxectos.indexOf(obx);
+          if (index > -1) {
+            arrayObxectos.splice(index, 1);
+
+            let marc = marcadores.splice(index, 1);
+            console.log("marc ");
+            console.log(marc);
+            marc.remove(map);
+          }
+
+          // card mb-2 shadow
+          let carta = document.body.getElementsByClassName(
+            "card " + obx.nome
+          )[0];
+          carta.remove();
+        }
+      }
+
+      localStorage.setItem("puntos", JSON.stringify(arrayObxectos));
     }
   }
 });
@@ -111,7 +147,6 @@ if ("geolocation" in navigator) {
         attribution:
           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }).addTo(map);
-
       map.on("click", onMapClick);
     },
     function (error) {
@@ -119,6 +154,15 @@ if ("geolocation" in navigator) {
     }
   );
 } else {
+  /* geolocation IS NOT available */
 }
 
-localStorage.removeItem("puntos");
+let puntos = JSON.parse(localStorage.getItem("puntos"));
+if (puntos != null) {
+  puntos = JSON.parse(localStorage.getItem("puntos"));
+  for (punto of puntos) {
+    crearCarta(punto.nome, punto.latitude, punto.lonxitude);
+  }
+}
+
+// localStorage.removeItem("puntos");
