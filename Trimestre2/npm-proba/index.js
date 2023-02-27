1083acccbd90e8a55c1762bb6b844826bff47956
a21inespg <a21inespg@iessanclemente.net>
2023-02-27 17:06:29 +0100

    s

--- Trimestre2/npm-proba/index.js
+++ Trimestre2/npm-proba/index.js
@@ -1,4 +1,4 @@
-import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+import cloneDeep from "lodash-es/cloneDeep.js";
 const obj1 = { a: 0, b: { c: 0 } };
 const obj2 = Object.assign({}, obj1);
 console.log(obj2); // { a: 0, b: { c: 0 } }
@@ -11,4 +11,3 @@ console.log(obj2); // { a: 2, b: { c: 0 } }
 obj2.b.c = 3;
 console.log(obj1); // { a: 1, b: { c: 3 } }
 console.log(obj2); // { a: 2, b: { c: 3 } }
-
