50e38f491364e006c91a07e8cafe1dad91a38316
a21inespg <a21inespg@iessanclemente.net>
2023-02-28 10:22:13 +0100

    s

--- Trimestre2/4_Vite/ex2_listaContactos/src/components/Contacto.vue
+++ Trimestre2/4_Vite/ex2_listaContactos/src/components/Contacto.vue
@@ -21,7 +21,15 @@ export default {
 </script>
 
 <template>
-  <div style="border: 1px solid black; border-radius: 20px; padding: 1.5em">
+  <div
+    style="
+      border: 1px solid black;
+      border-radius: 20px;
+      padding: 1.5em;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
+        0 6px 20px 0 rgba(0, 0, 0, 0.19); ;
+    "
+  >
     <h2>{{ nome }}</h2>
     <button @click="mostrar = !mostrar">{{ boton }}</button>
     <div v-show="mostrar">
